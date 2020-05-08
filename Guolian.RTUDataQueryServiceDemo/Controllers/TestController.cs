6d6ac3a6953b129c8f6e9310d879c00cbe4aa7e2
HYM <244781212@qq.com>
2020-05-08 17:22:19 +0800

    完结

---
+++ Guolian.RTUDataQueryServiceDemo/Controllers/TestController.cs
@@ -0,0 +1,51 @@
+﻿using Guolian.RTUDataQueryService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guolian.RTUDataQueryServiceDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController
+    {
+        private  QueryService queryService;
+        public TestController(QueryService queryService) 
+        {
+            this.queryService = queryService;
+        }
+
+        [HttpGet]
+        public async Task<object> Get() 
+        {
+            var test = await queryService.GetManagementInformation();
+            return test;
+        }
+        [HttpGet("{id}")]
+        public async Task<object> GetOne(int id) 
+        {
+            var test = await queryService.GetManagementInformation(id);
+            return test;
+        }
+        [HttpGet("{id}/management")]
+        public async Task<object> GetTwo(int id) 
+        {
+            var test = await queryService.GetEquipmentInformation(id);
+            return test;
+        }
+        [HttpGet("{id}/equipment")]
+        public async Task<object> GetThree(int id)
+        {
+            var test = await queryService.GetEquipmentInformationByID(id);
+            return test;
+        }
+        [HttpGet("{id}/history")]
+        public async Task<object> GetThree(int id,string year)
+        {
+            var test = await queryService.GetHistory(id,year);
+            return test;
+        }
+    }
+}
---
+++ Guolian.RTUDataQueryServiceDemo/Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Guolian.RTUDataQueryService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Guolian.RTUDataQueryServiceDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRTUClient(Configuration);
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                           new OpenApiInfo()
+                           {
+                               Title = $"读取服务",
+                               Version = "v1",
+                               Description = "读取服务",
+                           }
+                );
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                c.IgnoreObsoleteProperties();
+                c.IgnoreObsoleteActions();
+                c.EnableAnnotations();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/v1/swagger.json", "v1");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
